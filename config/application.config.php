e2e87c13559df4dc598485ae14ee14731b03438d
mockie <rifkimuhammad89@gmail.com>
2014-09-06 19:39:34 +0700

    homepage module

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'LearnZF2Ajax'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
