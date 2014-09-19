bab268fe4e2c748573b6450e103961b972d7d9e5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-09-19 22:53:20 +0700

    feedback

--- config/application.config.php
+++ config/application.config.php
@@ -3,7 +3,8 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
-        'LearnZF2Ajax'
+        'LearnZF2Ajax',
+        'LearnZF2FormUsage',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
--- module/LearnZF2Ajax/config/module.config.php
+++ module/LearnZF2Ajax/config/module.config.php
@@ -1,29 +1,9 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 return array(
     'router' => array(
         'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route'    => '/',
-                    'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
-                        'action'     => 'index',
-                    ),
-                ),
-            ),
-            // The following is a route to simplify getting started creating
-            // new controllers and actions without needing to create a new
-            // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
+
             'learnZF2Ajax' => array(
                 'type'    => 'Literal',
                 'options' => array(
@@ -52,18 +32,6 @@ return array(
             ),
         ),
     ),
-    'service_manager' => array(
-    ),
-    'translator' => array(
-        'locale' => 'en_US',
-        'translation_file_patterns' => array(
-            array(
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ),
-        ),
-    ),
     'controllers' => array(
         'invokables' => array(
             'LearnZF2Ajax\Controller\Index' => 'LearnZF2Ajax\Controller\IndexController'
@@ -72,13 +40,6 @@ return array(
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
-        ),
-    ),
-    // Placeholder for console routes
-    'console' => array(
-        'router' => array(
-            'routes' => array(
-            ),
         ),
     ),
 );
