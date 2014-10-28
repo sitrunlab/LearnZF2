25575320d842bd091e2a5e3a34f8184ff1ba30fd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-10-29 00:44:28 +0700

    - adding download link button view helper

---
+++ module/Application/src/Application/Controller/DownloadController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\Paginator\Paginator;
+use Zend\Paginator\Adapter\ArrayAdapter;
+
+class DownloadController extends AbstractActionController
+{
+    public function learnmoduleAction()
+    {
+        $module = $this->params()->fromQuery('module', '');
+        
+    }
+}
