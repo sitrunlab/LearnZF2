0a37cec3d600ced7fa66dd030bc159cce0b57f2a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-12-04 21:32:09 +0100

    Implemented data manipulation in Pagination module

--- module/LearnZF2Pagination/src/LearnZF2Pagination/Controller/IndexController.php
+++ module/LearnZF2Pagination/src/LearnZF2Pagination/Controller/IndexController.php
@@ -2,10 +2,17 @@
 namespace LearnZF2Pagination\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Paginator\Adapter\ArrayAdapter;
+use Zend\Paginator\Paginator;
 use Zend\View\Helper\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+    const ITEM_PER_PAGE = 5;
+
+    /**
+     * @var array
+     */
     protected $data;
 
     public function __construct(array $data)
@@ -15,6 +22,42 @@ class IndexController extends AbstractActionController
 
     public function indexAction()
     {
-        return new ViewModel(['data' => $this->data]);
+        $data = $this->getFilteredData();
+        $page = $this->params()->fromQuery('page', 1);
+        // Paginate filtered data
+        $paginator = new Paginator(new ArrayAdapter($data));
+        $paginator->setCurrentPageNumber($page)
+                  ->setItemCountPerPage(self::ITEM_PER_PAGE);
+
+        return new ViewModel(['data' => $paginator]);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getFilteredData()
+    {
+        // Filter by category
+        $category = $this->params()->fromQuery('category');
+        $data = isset($category) && array_key_exists($category, $this->data) ? $this->data[$category] : array_merge(
+            $this->data['movies'],
+            $this->data['books'],
+            $this->data['music']
+        );
+
+        // Filter by keyword
+        $keyword = $this->params()->fromQuery('keyword');
+        if (! isset($keyword)) {
+            return $data;
+        }
+
+        // Remove those elements which doesn't accomplish the keyword condition
+        foreach ($data as $key => $element) {
+            if (strpos($element['title'], $keyword) === false) {
+                unset($data[$key]);
+            }
+        }
+
+        return $data;
     }
 }
