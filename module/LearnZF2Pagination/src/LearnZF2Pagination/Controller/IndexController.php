6aadcda65105d4e496dbc51a4cd8f253cbad1c21
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-12-04 21:23:18 +0100

    Created Pagination\IndexControllerFactory

--- module/LearnZF2Pagination/src/LearnZF2Pagination/Controller/IndexController.php
+++ module/LearnZF2Pagination/src/LearnZF2Pagination/Controller/IndexController.php
@@ -2,11 +2,19 @@
 namespace LearnZF2Pagination\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Helper\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+    protected $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
     public function indexAction()
     {
-        return [];
+        return new ViewModel(['data' => $this->data]);
     }
 }
