5d8beffc57f734e78443d7dbac88f3cb0ba54dc1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-12-09 18:32:23 +0700

    update entity test set get only, no need mock

--- module/Application/test/ApplicationTest/Entity/ModuleEntityTest.php
+++ module/Application/test/ApplicationTest/Entity/ModuleEntityTest.php
@@ -7,42 +7,15 @@ use PHPUnit_Framework_TestCase;
 
 class ModuleEntityTest extends PHPUnit_Framework_TestCase
 {
-    public function testPersistModuleListAndGetIt()
+    public function testSetGetModuleListEntity()
     {
         $entity = new ModuleList();
-        $entity->setModuleName('foo');
-        $entity->setModuleDesc('bar');
-        $entity->setModuleRoute('baz');
-
-        $entityMock = $this->getMock('Application\Entity\ModuleList');
-        $entityMock->expects($this->once())
-            ->method('getId')
-            ->will($this->returnValue($entity->getId()));
-        $entityMock->expects($this->once())
-            ->method('getModuleName')
-            ->will($this->returnValue($entity->getModuleName()));
-        $entityMock->expects($this->once())
-            ->method('getModuleDesc')
-            ->will($this->returnValue($entity->getModuleDesc()));
-        $entityMock->expects($this->once())
-            ->method('getModuleRoute')
-            ->will($this->returnValue($entity->getModuleRoute()));
-
-        $entityManagerMock = $this->getMockBuilder('\Doctrine\Common\Persistence\ObjectManager')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $entityManagerMock->expects($this->once())
-            ->method('persist')
-            ->will($this->returnValue($entity));
-        $entityManagerMock->expects($this->once())
-            ->method('flush');
-
-        $entityManagerMock->persist($entity);
-        $entityManagerMock->flush();
-
-        $this->assertEquals($entity->getId(), $entityMock->getId());
-        $this->assertEquals($entity->getModuleName(), $entityMock->getModuleName());
-        $this->assertEquals($entity->getModuleDesc(), $entityMock->getModuleDesc());
-        $this->assertEquals($entity->getModuleRoute(), $entityMock->getModuleRoute());
+        $this->assertNull($entity->getId());
+        $this->assertSame($entity, $entity->setModuleName('foo'));
+        $this->assertSame('foo', $entity->getModuleName());
+        $this->assertSame($entity, $entity->setModuleDesc('bar'));
+        $this->assertSame('bar', $entity->getModuleDesc());
+        $this->assertSame($entity, $entity->setModuleRoute('baz'));
+        $this->assertSame('baz', $entity->getModuleRoute());
     }
 }
