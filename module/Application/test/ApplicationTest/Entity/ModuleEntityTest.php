2784a917313d05c5520a1a559ba85e0132586e16
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-12-09 13:10:31 +0700

    make coverage 100% again

--- module/Application/test/ApplicationTest/Entity/ModuleEntityTest.php
+++ module/Application/test/ApplicationTest/Entity/ModuleEntityTest.php
@@ -2,59 +2,47 @@
 
 namespace ApplicationTest\Entity;
 
+use Application\Entity\ModuleList;
 use PHPUnit_Framework_TestCase;
 
 class ModuleEntityTest extends PHPUnit_Framework_TestCase
 {
-    public function testFindGetOneData()
+    public function testPersistModuleListAndGetIt()
     {
-        $entity = $this->getMock('Application\Entity\ModuleList');
-        $entity->expects($this->once())
+        $entity = new ModuleList();
+        $entity->setModuleName('foo');
+        $entity->setModuleDesc('bar');
+        $entity->setModuleRoute('baz');
+
+        $entityMock = $this->getMock('Application\Entity\ModuleList');
+        $entityMock->expects($this->once())
             ->method('getId')
-            ->will($this->returnValue(1));
-        $entity->expects($this->once())
-            ->method('setModuleName')
-            ->with('LearnZF2FormUsage')
-            ->will($this->returnValue($entity));
-        $entity->expects($this->once())
+            ->will($this->returnValue($entity->getId()));
+        $entityMock->expects($this->once())
             ->method('getModuleName')
-            ->will($this->returnValue('LearnZF2FormUsage'));
-        $entity->expects($this->once())
-            ->method('setModuleDesc')
-            ->with('Learn Form Usage with ZF2')
-            ->will($this->returnValue($entity));
-        $entity->expects($this->once())
+            ->will($this->returnValue($entity->getModuleName()));
+        $entityMock->expects($this->once())
             ->method('getModuleDesc')
-            ->will($this->returnValue('Learn Form Usage with ZF2'));
-        $entity->expects($this->once())
-            ->method('setModuleRoute')
-            ->with('learn-zf2-form-usage')
-            ->will($this->returnValue($entity));
-        $entity->expects($this->once())
+            ->will($this->returnValue($entity->getModuleDesc()));
+        $entityMock->expects($this->once())
             ->method('getModuleRoute')
-            ->will($this->returnValue('learn-zf2-form-usage'));
+            ->will($this->returnValue($entity->getModuleRoute()));
 
-        $entityRepository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+        $entityManagerMock = $this->getMockBuilder('\Doctrine\Common\Persistence\ObjectManager')
             ->disableOriginalConstructor()
             ->getMock();
-        $entityRepository->expects($this->once())
-            ->method('find')
+        $entityManagerMock->expects($this->once())
+            ->method('persist')
             ->will($this->returnValue($entity));
+        $entityManagerMock->expects($this->once())
+            ->method('flush');
 
-        $entityManager = $this->getMockBuilder('\Doctrine\Common\Persistence\ObjectManager')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $entityManager->expects($this->once())
-            ->method('getRepository')
-            ->will($this->returnValue($entityRepository));
+        $entityManagerMock->persist($entity);
+        $entityManagerMock->flush();
 
-        $findId1 = $entityManager->getRepository('Application\Entity\ModuleList')->find(1);
-        $this->assertEquals(1, $findId1->getId());
-        $this->assertInstanceOf('Application\Entity\ModuleList', $findId1->setModuleName('LearnZF2FormUsage'));
-        $this->assertEquals('LearnZF2FormUsage', $findId1->getModuleName());
-        $this->assertInstanceOf('Application\Entity\ModuleList', $findId1->setModuleDesc('Learn Form Usage with ZF2'));
-        $this->assertEquals('Learn Form Usage with ZF2', $findId1->getModuleDesc());
-        $this->assertInstanceOf('Application\Entity\ModuleList', $findId1->setModuleRoute('learn-zf2-form-usage'));
-        $this->assertEquals('learn-zf2-form-usage', $findId1->getModuleRoute());
+        $this->assertEquals($entity->getId(), $entityMock->getId());
+        $this->assertEquals($entity->getModuleName(), $entityMock->getModuleName());
+        $this->assertEquals($entity->getModuleDesc(), $entityMock->getModuleDesc());
+        $this->assertEquals($entity->getModuleRoute(), $entityMock->getModuleRoute());
     }
 }
