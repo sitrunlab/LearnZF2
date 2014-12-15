7948ab7d01812435fc4f4a479308b21cbb460602
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-12-15 15:51:30 +0700

    add assertion

--- module/Application/test/ApplicationTest/Controller/ConsoleControllerTest.php
+++ module/Application/test/ApplicationTest/Controller/ConsoleControllerTest.php
@@ -28,8 +28,6 @@ class ConsoleControllerTest extends PHPUnit_Framework_TestCase
         $expected =  "HTTP/1.1 200 OK\r\n\r\n"
             ."[{\"login\":\"samsonasik\",\"id\":459648,\"avatar_url\":\"https://avatars.githubusercontent.com/u/459648?v=3\",\"gravatar_id\":\"\",\"url\":\"https://api.github.com/users/samsonasik\",\"html_url\":\"https://github.com/samsonasik\",\"followers_url\":\"https://api.github.com/users/samsonasik/followers\",\"following_url\":\"https://api.github.com/users/samsonasik/following{/other_user}\",\"gists_url\":\"https://api.github.com/users/samsonasik/gists{/gist_id}\",\"starred_url\":\"https://api.github.com/users/samsonasik/starred{/owner}{/repo}\",\"subscriptions_url\":\"https://api.github.com/users/samsonasik/subscriptions\",\"organizations_url\":\"https://api.github.com/users/samsonasik/orgs\",\"repos_url\":\"https://api.github.com/users/samsonasik/repos\",\"events_url\":\"https://api.github.com/users/samsonasik/events{/privacy}\",\"received_events_url\":\"https://api.github.com/users/samsonasik/received_events\",\"type\":\"User\",\"site_admin\":false,\"contributions\":168},{\"login\":\"acelaya\",\"id\":2719332,\"avatar_url\":\"https://avatars.githubusercontent.com/u/2719332?v=3\",\"gravatar_id\":\"\",\"url\":\"https://api.github.com/users/acelaya\",\"html_url\":\"https://github.com/acelaya\",\"followers_url\":\"https://api.github.com/users/acelaya/followers\",\"following_url\":\"https://api.github.com/users/acelaya/following{/other_user}\",\"gists_url\":\"https://api.github.com/users/acelaya/gists{/gist_id}\",\"starred_url\":\"https://api.github.com/users/acelaya/starred{/owner}{/repo}\",\"subscriptions_url\":\"https://api.github.com/users/acelaya/subscriptions\",\"organizations_url\":\"https://api.github.com/users/acelaya/orgs\",\"repos_url\":\"https://api.github.com/users/acelaya/repos\",\"events_url\":\"https://api.github.com/users/acelaya/events{/privacy}\",\"received_events_url\":\"https://api.github.com/users/acelaya/received_events\",\"type\":\"User\",\"site_admin\":false,\"contributions\":35},{\"login\":\"mockiemockiz\",\"id\":1708946,\"avatar_url\":\"https://avatars.githubusercontent.com/u/1708946?v=3\",\"gravatar_id\":\"\",\"url\":\"https://api.github.com/users/mockiemockiz\",\"html_url\":\"https://github.com/mockiemockiz\",\"followers_url\":\"https://api.github.com/users/mockiemockiz/followers\",\"following_url\":\"https://api.github.com/users/mockiemockiz/following{/other_user}\",\"gists_url\":\"https://api.github.com/users/mockiemockiz/gists{/gist_id}\",\"starred_url\":\"https://api.github.com/users/mockiemockiz/starred{/owner}{/repo}\",\"subscriptions_url\":\"https://api.github.com/users/mockiemockiz/subscriptions\",\"organizations_url\":\"https://api.github.com/users/mockiemockiz/orgs\",\"repos_url\":\"https://api.github.com/users/mockiemockiz/repos\",\"events_url\":\"https://api.github.com/users/mockiemockiz/events{/privacy}\",\"received_events_url\":\"https://api.github.com/users/mockiemockiz/received_events\",\"type\":\"User\",\"site_admin\":false,\"contributions\":33}]";
 
-        $this->httpClientadapter->setResponse($expected);
-
         $httpClient = new HttpClient();
         $httpClient->setAdapter(get_class($this->httpClientadapter));
         $httpClient->setUri('http://www.domain.com/');
@@ -48,6 +46,8 @@ class ConsoleControllerTest extends PHPUnit_Framework_TestCase
         );
 
         $controller->getcontributorsAction();
+
+        $this->assertContains("samsonasik", file_get_contents('foo.pson'));
         @unlink('foo.pson');
     }
 
@@ -57,8 +57,6 @@ class ConsoleControllerTest extends PHPUnit_Framework_TestCase
         $consoleAdapter = new $adapter();
 
         $expected =  "HTTP/1.1 400 Bad Request\r\n\r\n";
-
-        $this->httpClientadapter->setResponse($expected);
 
         $httpClient = new HttpClient();
         $httpClient->setAdapter(get_class($this->httpClientadapter));
