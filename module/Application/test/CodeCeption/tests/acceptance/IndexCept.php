3e346c54066c95862b3caa2908fbf1896d8adb57
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-12-24 12:16:59 +0700

    added more assertion for click href "Fork us on Github" and switch open new window

--- module/Application/test/CodeCeption/tests/acceptance/IndexCept.php
+++ module/Application/test/CodeCeption/tests/acceptance/IndexCept.php
@@ -3,3 +3,11 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('get home page');
 $I->amOnPage('/');
 $I->see('Welcome to LearnZF2 site');
+$I->canSeeLink('Fork us on GitHub »');
+$I->click('Fork us on GitHub »');
+$I->executeInSelenium(function (\Webdriver $webdriver) {
+    $handles = $webdriver->getWindowHandles();
+    $last_window = end($handles);
+    $webdriver->switchTo()->window($last_window);
+});
+$I->switchToWindow();
