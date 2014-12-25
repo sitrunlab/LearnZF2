f60867e80dda6fdbff060aad62c4f552b7dafda8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-12-25 16:49:24 +0700

    added test

---
+++ module/Application/test/ApplicationTest/Controller/SitemapControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class SitemapControllerTest extends AbstractHttpControllerTestCase
+{
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            include dirname(dirname(dirname(dirname(dirname(__DIR__))))).'/config/application.config.php'
+        );
+        parent::setUp();
+    }
+
+    public function testGetXmlPage()
+    {
+        $this->dispatch('/sitemapxml');
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('Application');
+        $this->assertControllerName('Application\Controller\Sitemap');
+        $this->assertControllerClass('SitemapController');
+        $this->assertMatchedRouteName('sitemapxml');
+
+        $this->assertEquals('Content-Type: text/xml', $this->getResponseHeader('Content-Type')->toString());
+    }
+}
