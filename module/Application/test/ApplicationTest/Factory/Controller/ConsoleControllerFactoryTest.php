b7692b18f3bc638bdd3dfbc9b86013e7296088aa
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-01-18 20:59:39 +0100

    Created ConsoleControllerFactory test case

---
+++ module/Application/test/ApplicationTest/Factory/Controller/ConsoleControllerFactoryTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace ApplicationTest\Factory\Controller;
+
+use Application\Factory\Controller\ConsoleControllerFactory;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Console\Adapter\Posix;
+use Zend\Mvc\Controller\ControllerManager;
+use Zend\ServiceManager\ServiceManager;
+
+class ConsoleControllerFactoryTest extends TestCase
+{
+    /**
+     * @var ConsoleControllerFactory
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new ConsoleControllerFactory();
+    }
+
+    public function testCreateService()
+    {
+        $sm = new ServiceManager();
+        $sm->setService('Console', new Posix());
+        $sm->setService('Config', []);
+        $cm = new ControllerManager();
+        $cm->setServiceLocator($sm);
+
+        $controller = $this->factory->createService($cm);
+        $this->assertInstanceOf('Application\Controller\ConsoleController', $controller);
+    }
+}
