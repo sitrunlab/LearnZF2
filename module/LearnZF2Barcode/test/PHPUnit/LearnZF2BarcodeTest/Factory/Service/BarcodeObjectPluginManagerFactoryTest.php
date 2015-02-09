cf6da5233adfe2ddd5a7d012c3e2eab9fd6dce10
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-02-10 06:44:50 +0700

    add BarcodeObjectPluginManagerFactory test

---
+++ module/LearnZF2Barcode/test/PHPUnit/LearnZF2BarcodeTest/Factory/Service/BarcodeObjectPluginManagerFactoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LearnZF2BarcodeTest\Factory\Service;
+
+use LearnZF2Barcode\Factory\Service\BarcodeObjectPluginManagerFactory;
+use PHPUnit_Framework_TestCase;
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * @author Abdul Malik Ikhsan <samsonasik@gmail.com>
+ */
+class BarcodeObjectPluginManagerFactoryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ServiceManager
+     */
+    protected $services;
+
+    /**
+     * @var BarcodeObjectPluginManagerFactory
+     */
+
+    public function setUp()
+    {
+        $this->services = new ServiceManager();
+        $this->services->setService('Config', []);
+        $this->factory  = new BarcodeObjectPluginManagerFactory();
+    }
+
+    public function testInstanceOfObjectPluginManager()
+    {
+        $manager = $this->factory->createService($this->services);
+        $this->assertInstanceof('Zend\Barcode\ObjectPluginManager', $manager);
+    }
+
+    public function testGetServiceViaManager()
+    {
+        $manager = $this->factory->createService($this->services);
+        $manager->get('codabar');
+    }
+}
