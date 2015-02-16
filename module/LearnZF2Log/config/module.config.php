c02326dbf8500f1e2525e6e723eebc27fa05203d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-02-16 18:24:37 +0700

    basic structure of module

--- module/LearnZF2Log/config/module.config.php
+++ module/LearnZF2Log/config/module.config.php
@@ -1,3 +1,29 @@
 <?php
-return array(
-);
+
+return [
+    'router' => [
+        'routes' => [
+            'learn-zf2-log' => [
+                'type'    => 'Literal',
+                'options' => [
+                    'route'    => '/learn-zf2-log',
+                    'defaults' => [
+                        '__NAMESPACE__' => 'LearnZF2Log\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'invokables' => [
+            'LearnZF2Log\Controller\Index' => 'LearnZF2Log\Controller\IndexController',
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'learnzf2log' => __DIR__.'/../view',
+        ],
+    ],
+];
