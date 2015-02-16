ebea3e62a03a8d57b38c0db76397eefa65e9a7d5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-02-16 18:33:54 +0700

    cs

--- module/LearnZF2Log/Module.php
+++ module/LearnZF2Log/Module.php
@@ -21,7 +21,7 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__.'/config/module.config.php';
     }
 
     public function getAutoloaderConfig()
@@ -29,7 +29,7 @@ class Module
         return [
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__.'/src/'.__NAMESPACE__,
                 ],
             ],
         ];
