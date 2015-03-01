df39b216a3bc3e4460f8162d835e63036d0ff7f6
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-03-01 21:43:35 +0100

    Added ModuleTest in navigation module

---
+++ module/LearnZF2Navigation/test/PHPUnit/LearnZF2NavigationTest/ModuleTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace LearnZF2NavigationTest;
+
+use LearnZF2Navigation\Module;
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Loader\StandardAutoloader;
+
+class ModuleTest extends TestCase
+{
+    /**
+     * @var Module
+     */
+    private $module;
+
+    public function setUp()
+    {
+        $this->module = new Module();
+    }
+
+    public function testGetConfig()
+    {
+        $config = $this->module->getConfig();
+        $expected = include __DIR__ . '/../../../config/module.config.php';
+        $this->assertEquals($expected, $config);
+    }
+
+    public function testGetAutoloaderConfig()
+    {
+        $config = $this->module->getAutoloaderConfig();
+        $this->assertTrue(is_array($config));
+        $this->assertArrayHasKey('Zend\Loader\StandardAutoloader', $config);
+    }
+}
