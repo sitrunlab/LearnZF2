ca786402f6d9c67ee4cfb76edec92c13ec7a5ffe
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-04-07 09:41:37 +0700

    re-fix sensiolabs

--- module/LearnZF2Pagination/src/LearnZF2Pagination/Exception/RuntimeException.php
+++ module/LearnZF2Pagination/src/LearnZF2Pagination/Exception/RuntimeException.php
@@ -3,7 +3,8 @@
 namespace LearnZF2Pagination\Exception;
 
 use RuntimeException as BaseRuntimeException;
+use LearnZF2Pagination\Exception;
 
-class RuntimeException extends BaseRuntimeException implements ExceptionInterface
+class RuntimeException extends BaseRuntimeException implements Exception\ExceptionInterface
 {
 }
