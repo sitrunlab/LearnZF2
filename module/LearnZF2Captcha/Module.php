24ef550c752e4b89b745c896b80db6b7a8787840
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-08-02 07:20:22 +0700

    cs

--- module/LearnZF2Captcha/Module.php
+++ module/LearnZF2Captcha/Module.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace LearnZF2Captcha;
 
 class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__.'/config/module.config.php';
     }
 
     public function getAutoloaderConfig()
@@ -13,7 +14,7 @@ class Module
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ => __DIR__.'/src/'.__NAMESPACE__,
                 ),
             ),
         );
