701372b000f35d4d7be628c8a4c651d384331712
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-08-17 04:52:39 +0700

    adding tests

---
+++ module/LearnZF2Captcha/test/PHPUnit/Bootstrap.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace test;
+
+use Zend\Loader\AutoloaderFactory;
+use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\ServiceManager\ServiceManager;
+use RuntimeException;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+/**
+ * Test bootstrap, for setting up autoloading.
+ */
+class Bootstrap
+{
+    protected static $serviceManager;
+
+    public static function init()
+    {
+        $zf2ModulePaths = [dirname(dirname(__DIR__))];
+        if (($path = static::findParentPath('vendor'))) {
+            $zf2ModulePaths[] = $path;
+        }
+        if (($path = static::findParentPath('module')) !== $zf2ModulePaths[0]) {
+            $zf2ModulePaths[] = $path;
+        }
+
+        static::initAutoloader();
+
+        // use ModuleManager to load this module and it's dependencies
+        $config = [
+            'module_listener_options' => [
+                'module_paths' => $zf2ModulePaths,
+            ],
+            'modules' => [
+                'LearnZF2Captcha',
+            ],
+        ];
+
+        $serviceManager = new ServiceManager(new ServiceManagerConfig());
+        $serviceManager->setService('ApplicationConfig', $config);
+        $serviceManager->get('ModuleManager')->loadModules();
+        static::$serviceManager = $serviceManager;
+    }
+
+    public static function chroot()
+    {
+        $rootPath = dirname(static::findParentPath('module'));
+        chdir($rootPath);
+    }
+
+    public static function getServiceManager()
+    {
+        return static::$serviceManager;
+    }
+
+    protected static function initAutoloader()
+    {
+        $vendorPath = static::findParentPath('vendor');
+
+        $zf2Path = getenv('ZF2_PATH');
+        if (!$zf2Path) {
+            if (defined('ZF2_PATH')) {
+                $zf2Path = ZF2_PATH;
+            } elseif (is_dir($vendorPath.'/ZF2/library')) {
+                $zf2Path = $vendorPath.'/ZF2/library';
+            } elseif (is_dir($vendorPath.'/zendframework/zendframework/library')) {
+                $zf2Path = $vendorPath.'/zendframework/zendframework/library';
+            }
+        }
+
+        if (!$zf2Path) {
+            throw new RuntimeException(
+                'Unable to load ZF2. Run `php composer.phar install` or'
+                .' define a ZF2_PATH environment variable.'
+            );
+        }
+
+        if (file_exists($vendorPath.'/autoload.php')) {
+            include $vendorPath.'/autoload.php';
+        }
+
+        include $zf2Path.'/Zend/Loader/AutoloaderFactory.php';
+        AutoloaderFactory::factory([
+            'Zend\Loader\StandardAutoloader' => [
+                'autoregister_zf' => true,
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__.'/'.__NAMESPACE__,
+                ],
+            ],
+        ]);
+    }
+
+    protected static function findParentPath($path)
+    {
+        $dir = __DIR__;
+        $previousDir = '.';
+        while (!is_dir($dir.'/'.$path)) {
+            $dir = dirname($dir);
+            if ($previousDir === $dir) {
+                return false;
+            }
+            $previousDir = $dir;
+        }
+
+        return $dir.'/'.$path;
+    }
+}
+
+Bootstrap::init();
+Bootstrap::chroot();
---
+++ module/LearnZF2Captcha/test/PHPUnit/LearnZF2CaptchaTest/Factory/Controller/AclControllerFactoryTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace LearnZF2CaptchaTest\Factory\Controller;
+
+use PHPUnit_Framework_TestCase;
+use LearnZF2Captcha\Factory\Controller\CaptchaControllerFactory;
+use Zend\Mvc\Controller\ControllerManager;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class CaptchaControllerFactoryTest extends PHPUnit_Framework_TestCase
+{
+    /** @var CaptchaControllerFactory */
+    protected $factory;
+
+    /** @var ControllerManager */
+    protected $controllerManager;
+
+    /** @var ServiceLocatorInterface */
+    protected $serviceLocator;
+
+    public function setUp()
+    {
+        /** @var ControllerManager $controllerManager */
+        $controllerManager = $this->getMock('Zend\Mvc\Controller\ControllerManager');
+        $this->controllerManager = $controllerManager;
+
+        /** @var ServiceLocatorInterface $serviceLocator */
+        $serviceLocator = $this->getMock('Zend\ServiceManager\ServiceLocatorInterface');
+        $this->serviceLocator = $serviceLocator;
+
+        $controllerManager->expects($this->any())
+                          ->method('getServiceLocator')
+                          ->willReturn($serviceLocator);
+
+        $factory = new CaptchaControllerFactory();
+        $this->factory = $factory;
+    }
+
+    public function testCreateServiceWithServiceLocator()
+    {
+        $this->doTestCreateService($this->serviceLocator);
+    }
+
+    public function testCreateServiceWithControllerManager()
+    {
+        $this->doTestCreateService($this->controllerManager);
+    }
+
+    private function doTestCreateService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = include __DIR__ . '/../../config/captcha.config.php';
+        $serviceLocator->expects($this->at(0))
+                       ->method('get')
+                       ->with('Config')
+                       ->willReturn($config);
+        $application = $this->getMockBuilder('Zend\Mvc\Application')
+                            ->disableOriginalConstructor()
+                            ->getMock();
+        $mvcEvent    = $this->getMockBuilder('Zend\Mvc\MvcEvent')
+                            ->disableOriginalConstructor()
+                            ->getMock();
+        $application->expects($this->once())
+                    ->method('getMvcEvent')
+                    ->willReturn($mvcEvent);
+
+        $services->expects($this->at(2))
+                 ->method('get')
+                 ->with('Application')
+                 ->willReturn($application);
+
+        $result = $this->factory->createService($serviceLocator);
+        $this->assertInstanceOf('LearnZF2Captcha\Controller\CaptchaController', $result);
+    }
+}
