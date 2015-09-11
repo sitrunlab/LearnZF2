80d996879205f7e17ef1205055c101c8af97a081
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-09-11 14:11:00 +0700

    update exception wording

--- module/LearnZF2I18n/test/PHPunit/Bootstrap.php
+++ module/LearnZF2I18n/test/PHPunit/Bootstrap.php
@@ -83,8 +83,7 @@ class Bootstrap
 
         if (! class_exists('Zend\Loader\AutoloaderFactory')) {
             throw new RuntimeException(
-                'Unable to load ZF2. Run `php composer.phar install` or'
-                .' define a ZF2_PATH environment variable.'
+                'Unable to load ZF2. Run `php composer.phar install`'
             );
         }
 
