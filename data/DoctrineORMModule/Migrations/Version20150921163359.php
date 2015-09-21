efe35ca66ecfaa60428724de0dd63d328e62875b
Stanimir Dimitrov <stanimirdim92@gmail.com>
2015-09-21 19:43:47 +0300

    doctrine migration

--- data/DoctrineORMModule/Migrations/Version20150921163359.php
+++ data/DoctrineORMModule/Migrations/Version20150921163359.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150921163359 extends AbstractMigration
+class Version20150606222937 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -16,7 +16,10 @@ class Version20150921163359 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("
+            INSERT INTO `module_list` (`module_name`, `module_desc`, `module_route`) VALUES
+            ('LearnZF2AjaxImageGallery', 'Learn Ajax Image Gallery with ZF2', 'learn-zf2-ajax-image-gallery');");
     }
 
     /**
@@ -25,6 +28,8 @@ class Version20150921163359 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql("
+            DELETE FROM `module_list` where `module_name` = 'LearnZF2AjaxImageGallery'");
     }
 }
